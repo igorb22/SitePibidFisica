190b06776eca19e6efa5d4128a9aa1d110d096e0
igorb22 <igorb22@live.com>
2019-12-26 10:40:08 -0300

    adicionando projeto

---
+++ PibidFisica/PibidFisica/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PibidFisica.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
